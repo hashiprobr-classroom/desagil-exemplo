a7934e2ab1130709fd3c4f5716743880c4eb80f0
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2022-05-02 12:56:28 -0300

    Add basis for REST requests

--- frontend/components/cats/Form.js
+++ frontend/components/cats/Form.js
@@ -1,12 +1,89 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { Button, TextInput } from 'react-native-paper';
+
+import DropDown from '@hashiprobr/react-native-paper-dropdown';
+import { DateTimePicker } from '@hashiprobr/react-native-paper-datetimepicker';
+
+import useRest from '@hashiprobr/expo-use-rest';
 
 import styles from '../../styles/cats/Form.json';
 
 export default function Form(props) {
+    const { navigation, route } = props;
+
+    const params = route.params;
+
+    const list = [
+        { label: 'Yellow', value: 'YELLOW' },
+        { label: 'Green', value: 'GREEN' },
+        { label: 'Blue', value: 'BLUE' },
+    ];
+
+    const [name, setName] = useState('');
+    const [breed, setBreed] = useState('');
+    const [eye, setEye] = useState('');
+    const [birth, setBirth] = useState(new Date());
+
+    useEffect(() => {
+        const title = params ? params.name : 'Add new cat';
+
+        navigation.setOptions({ title: title });
+
+    }, [params]);
+
+    const client = useRest('http://___.___.___.___:____');
+
+    function onChangeName(text) {
+        setName(text);
+    }
+
+    function onChangeBreed(text) {
+        setBreed(text);
+    }
+
+    function onChangeEye(value) {
+        setEye(value);
+    }
+
+    function onChangeBirth(date) {
+        setBirth(date);
+    }
+
+    async function onPressAdd() {
+        const cat = {
+            name: name,
+            breed: breed,
+            eye: eye,
+            birth: birth.getTime(),
+        };
+
+        let key;
+        try {
+            key = await client.post('/cat', cat);
+        } catch (error) {
+            console.error(error);
+        }
+
+        if (key) {
+            console.log(key);
+            navigation.navigate('List');
+        }
+    }
+
     return (
-        <View style={styles.container}>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <TextInput style={styles.input} label="Name" value={name} onChangeText={onChangeName} />
+
+            <TextInput style={styles.input} label="Breed" value={breed} onChangeText={onChangeBreed} />
+
+            <DropDown style={styles.input} list={list} label="Eye" value={eye} onChangeValue={onChangeEye} />
+
+            <DateTimePicker style={styles.input} type="date" label="Birth" value={birth} onChangeDate={onChangeBirth} />
+
+            <Button mode="outlined" onPress={onPressAdd}>Add</Button>
+        </SafeAreaView>
     );
 }
--- frontend/components/cats/List.js
+++ frontend/components/cats/List.js
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
+
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { FAB, ActivityIndicator } from 'react-native-paper';
+
+import useRest from '@hashiprobr/expo-use-rest';
+
+import { map } from '../../tools';
 
 import styles from '../../styles/cats/List.json';
 
 export default function List(props) {
+    const { navigation } = props;
+
+    useEffect(async () => {
+        let body;
+        try {
+            body = await client.get('/cat/list');
+        } catch (error) {
+            console.error(error);
+        }
+        if (body) {
+            setCats(body);
+        }
+    }, []);
+
+    const client = useRest('http://___.___.___.___:____');
+
+    function renderCat(cat) {
+        return (
+            <View style={styles.item}>
+            </View>
+        )
+    }
+
+    function onPressFAB() {
+        navigation.navigate('Form');
+    }
+
+    const [cats, setCats] = useState(null);
+
     return (
-        <View style={styles.container}>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.scroll}>
+                {cats !== null && (
+                    map(cats, renderCat)
+                )}
+            </ScrollView>
+            <FAB style={styles.fab} icon="plus" onPress={onPressFAB} />
+        </SafeAreaView >
     );
 }
